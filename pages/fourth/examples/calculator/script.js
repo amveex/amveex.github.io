b2270acb7eb7bd64b0d9f58f33ebccc513bd1a02
amveex <amveex@gmail.com>
2026-03-25 05:57:40 +0300

    improved calculator

--- pages/fourth/examples/calculator/script.js
+++ pages/fourth/examples/calculator/script.js
@@ -1,18 +1,64 @@
 let field = document.getElementById("FIELD");
+let A = null;
+let numA = null;
+let B = null;
+let numB = null;
+let sum = null;
+let symbol = null;
 
-function pick(symbol) {
-    let value = symbol;
-    field.value += value;
-}
 
 function reset() {
     field.value = "";
+    A = null;
+    numA = null;
+    B = null;
+    numB = null;
+    sum = null;
+    symbol = null;
 }
 
+
+function pickNumber(number) {
+    field.value += number;
+}
+
+
+function pickOperator(operator) {
+    symbol = operator;
+    
+    if (A == null) {
+        A = field.value;
+    }
+
+    field.value = "";
+}
+
+
 function calculate() {
-    try {
-        field.value = eval(field.value);
-    } catch (err) {
-        field.value = "Error";
+    B = field.value;
+    numA = Number(A);
+    numB = Number(B);
+
+    switch (symbol) {
+        case '+':
+            sum = numA + numB;
+            field.value = sum;
+            A = sum;
+            break;
+        case '-':
+            sum = numA - numB;
+            field.value = sum;
+            A = sum;
+            break;
+        case '*':
+            sum = numA * numB;
+            field.value = sum;
+            A = sum;
+            break;
+        case '/':
+            sum = numA / numB;
+            field.value = sum;
+            A = sum;
+            break;
     }
 }
